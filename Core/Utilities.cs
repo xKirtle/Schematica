75f16720d323aa00d19e355efd7dac9dd3c6d041
Kirtle <xkirtle26@gmail.com>
2022-08-05 14:55:52 +0100

    Read schematica's file names

---
+++ Core/Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Schematica.Core;
+
+public static class Utilities
+{
+    private static Regex FileNamesRegex = new Regex("(?:(?!\\\\).)+$");
+    public static List<string> FileNamesInDirectory(string directoryFullPath) {
+        return Directory.GetFiles(directoryFullPath).Select(x => FileNamesRegex.Match(x).Value.Replace(".schematica", "")).ToList();
+    }
+}
