2976922eecaec8cf26560e27042ea1869e29c840
Kirtle <xkirtle26@gmail.com>
2022-08-05 02:10:27 +0100

    Schematica Preview (Skeleton)

---
+++ Common/SchematicaPreview.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Schematica.Common.DataStructures;
+using Terraria;
+
+namespace Schematica.Common;
+
+public class SchematicaPreview
+{
+    public static void DrawPreview(SchematicData data, Vector2 position, float scale = 1f) {
+        // GraphicsDevice gd = Main.graphics.GraphicsDevice;
+        // SpriteBatch spriteBatch = Main.spriteBatch;
+    }
+}
