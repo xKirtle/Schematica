24427b13606624875f123dbd6e3e46206cf08a18
Kirtle <xkirtle26@gmail.com>
2022-08-05 02:07:59 +0100

    Generic UI System

---
+++ Common/Enums/VanillaInterfaceLayer.cs
@@ -0,0 +1,68 @@
+﻿namespace Schematica.Common.Enums;
+
+public enum VanillaInterfaceLayer
+{
+    Interface_Logic_1,
+    MP_Player_Names,
+    Emote_Bubbles,
+    Entity_Markers,
+    Smart_Cursor_Targets,
+    Laser_Ruler,
+    Ruler,
+    Gamepad_Lock_On,
+    Tile_Grid_Option,
+    Town_NPC_House_Banners,
+    Hide_UI_Toggle,
+    Wire_Selection,
+    Capture_Manager_Check,
+    Ingame_Options,
+    Fancy_UI,
+    Achievement_Complete_Popups,
+    Entity_Health_Bars,
+    Invasion_Progress_Bars,
+    Map_And_Minimap,
+    Diagnose_Net,
+    Diagnose_Video,
+    Sign_Tile_Bubble,
+    Hair_Window,
+    Dresser_Window,
+    NPC_And_Sign_Dialog,
+    Interface_Logic_2,
+    Resource_Bars,
+    Interface_Logic_3,
+    Inventory,
+    Info_Accessories_Bar,
+    Radial_Hotbars,
+    Mouse_Text,
+    Player_Chat,
+    Death_Text,
+    Cursor,
+    Debug_Stuff,
+    Mouse_Item_And_NPC_Head,
+    Mouse_Over,
+    Interact_Item_Icon,
+    Interface_Logic_4
+}
+
+public static class _
+{
+    public static string Stringify(this VanillaInterfaceLayer layer) {
+        string layerName = "";
+        switch (layer) {
+            case VanillaInterfaceLayer.Map_And_Minimap:
+                layerName = "Map / Minimap";
+                break;
+            case VanillaInterfaceLayer.NPC_And_Sign_Dialog:
+                layerName = "NPC / Sign Dialog";
+                break;
+            case VanillaInterfaceLayer.Mouse_Item_And_NPC_Head:
+                layerName = "Mouse Item / NPC Head";
+                break;
+            default:
+                layerName = layer.ToString().Replace("_", "");
+                break;
+        }
+
+        return $"Vanilla: {layerName}";
+    }
+}
---
+++ Common/Systems/GenericUISystem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Schematica.Common.Enums;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.UI;
+
+namespace Schematica.Common.Systems;
+
+[Autoload(Side = ModSide.Client)]
+public abstract class UISystem<T> : ModSystem where T : UIState, new()
+{
+    public UserInterface userInterface;
+    public T uiState;
+    public virtual string VanillaInterfaceLayer { get; protected set; } = Enums.VanillaInterfaceLayer.Ruler.Stringify();
+    public virtual string InterfaceLayerName { get; protected set; } = typeof(T).Name;
+    public virtual InterfaceScaleType InterfaceScaleType { get; protected set; } = InterfaceScaleType.None;
+
+    protected GameTime lastUpdateUiGameTime;
+
+    public override void Load() {
+        userInterface = new UserInterface();
+        uiState = new T();
+        userInterface.SetState(uiState);
+    }
+
+    public override void UpdateUI(GameTime gameTime) {
+        lastUpdateUiGameTime = gameTime;
+        if (userInterface?.CurrentState != null)
+            userInterface.Update(gameTime);
+    }
+    
+    public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers) {
+        //https://github.com/tModLoader/tModLoader/wiki/Vanilla-Interface-layers-values
+        int interfaceLayer = layers.FindIndex(layer => layer.Name.Equals(VanillaInterfaceLayer));
+        if (interfaceLayer != -1)
+            layers.Insert(interfaceLayer, new LegacyGameInterfaceLayer($"Schematica: {InterfaceLayerName}", DrawMethod, InterfaceScaleType));
+    }
+
+    protected virtual bool DrawMethod() {
+        if (lastUpdateUiGameTime != null && userInterface?.CurrentState != null)
+            userInterface.Draw(Main.spriteBatch, lastUpdateUiGameTime);
+
+        return true;
+    }
+
+    public void Activate() {
+        if (userInterface?.CurrentState != uiState)
+            userInterface?.SetState(uiState);
+    }
+
+    public void Deactivate() {
+        if (userInterface?.CurrentState != null)
+            userInterface?.SetState(null);
+    }
+}
