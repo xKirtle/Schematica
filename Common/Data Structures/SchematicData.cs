f1da9b02f166270e440d823bb5989ba03491c135
Kirtle <xkirtle26@gmail.com>
2022-08-05 02:08:55 +0100

    Schematic + Tile Data structures

---
+++ Common/Data Structures/SchematicData.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Schematica.Core.JsonConverters;
+using Terraria;
+using Terraria.Graphics.Capture;
+using Terraria.ModLoader;
+
+namespace Schematica.Common.DataStructures;
+
+[JsonConverter(typeof(SchematicDataConverter))]
+public class SchematicData
+{
+    public string Name { get; internal set; }
+    public Point Size { get; internal set; }
+
+    public List<TileData> data;
+    
+    private static Point EdgeA => CaptureInterface.EdgeA;
+    private static Point EdgeB => CaptureInterface.EdgeB;
+    private static string SavePath = $@"{Path.Combine(Main.SavePath)}\Schematica";
+    
+    //Bring up UI popup to ask for a name before saving
+    public static void SaveSchematic(string fileName = null) {
+        SchematicData schematic = new SchematicData();
+        try {
+            schematic.Name = fileName ?? GetNextDefaultSchematicName();
+            schematic.Size = new Point(Math.Abs(EdgeA.X - EdgeB.X) + 1, Math.Abs(EdgeA.Y - EdgeB.Y) + 1);
+
+            int numElements = schematic.Size.X * schematic.Size.Y;
+            Point minEdge = new Point(Math.Min(EdgeA.X, EdgeB.X), Math.Min(EdgeA.Y, EdgeB.Y));
+            schematic.data = new List<TileData>();
+            
+            for (int j = 0; j < schematic.Size.Y; j++)
+                for (int i = 0; i < schematic.Size.X; i++) {
+                    Tile tile = Main.tile[minEdge.X + i, minEdge.Y + j];
+                    schematic.data.Add(new TileData(tile));
+                }
+
+            Directory.CreateDirectory(SavePath);
+            
+            string path = $@"{SavePath}\{schematic.Name}.json";
+            string json = JsonConvert.SerializeObject(schematic);
+            string schematicPath = $@"{SavePath}\{schematic.Name}.schematica";
+
+            if (File.Exists(schematicPath))
+                File.Delete(schematicPath);
+
+            File.WriteAllText(path, json);
+            using var zipArchive = ZipFile.Open(schematicPath, ZipArchiveMode.Create);
+            zipArchive.CreateEntryFromFile(path, schematic.Name + ".json");
+            zipArchive.Dispose();
+
+            File.Delete(path);
+            
+            schematic = null;
+            GC.Collect();
+            GC.Collect();
+        }
+        catch (Exception e) { }
+    }
+    
+    public static void LoadSchematic(string filename) {
+        string path = $@"{SavePath}\{filename}";
+
+        using var zipArchive = new ZipArchive(new MemoryStream(File.ReadAllBytes($"{path}.schematica")));
+        Stream stream = zipArchive.Entries[0].Open();
+
+        using StreamReader sr = new StreamReader(stream);
+        using JsonTextReader reader = new JsonTextReader(sr);
+        JsonSerializer serializer = new JsonSerializer();
+        SchematicData schematic =  serializer.Deserialize<SchematicData>(reader);
+
+        Console.WriteLine($"{schematic.Name} {schematic.Size}");
+    }
+
+    //Method to read used folder for saves and detect what's the next integer for naming it can use (for the default naming scheme)
+    public static string GetNextDefaultSchematicName() {
+
+        return "DefaultName";
+    }
+}
---
+++ Common/Data Structures/TileData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Schematica.Core.JsonConverters;
+using Terraria;
+
+namespace Schematica.Common.DataStructures;
+
+[JsonConverter(typeof(TileDataConverter))]
+public class TileData
+{
+    public ushort TType;
+    public ushort WType;
+    public byte LAmount;
+    public byte LFlags;
+    public short TFrameX;
+    public short TFrameY;
+    public int TBitpack;
+    // public int TNonFrameBits => (int) (TBitpack & 0xFF001FFF);
+
+    public TileData() { }
+    
+    public TileData(Tile tile) {
+        TType = tile.Get<TileTypeData>().Type;
+        WType = tile.Get<WallTypeData>().Type;
+        
+        LiquidData lData = tile.Get<LiquidData>();
+        LAmount = lData.Amount;
+        LFlags = (byte) TileDataPacking.Pack(lData.LiquidType, LFlags, 0, 6);
+        LFlags = (byte) TileDataPacking.SetBit(lData.SkipLiquid, LFlags, 6);
+        LFlags = (byte) TileDataPacking.SetBit(lData.CheckingLiquid, LFlags, 7);
+
+
+        TileWallWireStateData data = tile.Get<TileWallWireStateData>();
+        TFrameX = data.TileFrameX;
+        TFrameY = data.TileFrameY;
+        
+        TBitpack = TileDataPacking.SetBit(data.HasTile, TBitpack, 0);
+        TBitpack = TileDataPacking.SetBit(data.IsActuated, TBitpack, 1);
+        TBitpack = TileDataPacking.SetBit(data.HasActuator, TBitpack, 2);
+
+        TBitpack = TileDataPacking.Pack(data.TileColor, TBitpack, 3, 5);
+        TBitpack = TileDataPacking.Pack(data.WallColor, TBitpack, 8, 5);
+        
+        TBitpack = TileDataPacking.Pack(data.TileFrameNumber, TBitpack, 13, 2);
+        TBitpack = TileDataPacking.Pack(data.WallFrameNumber, TBitpack, 15, 2);
+        
+        TBitpack = TileDataPacking.Pack(data.WallFrameX / 36, TBitpack, 17, 4);
+        TBitpack = TileDataPacking.Pack(data.WallFrameY / 36, TBitpack, 21, 3);
+        
+        TBitpack = TileDataPacking.SetBit(data.IsHalfBlock, TBitpack, 24);
+        TBitpack = TileDataPacking.Pack((int) data.Slope, TBitpack, 25, 3);
+
+        TBitpack = TileDataPacking.Pack(data.WireData, TBitpack, 28, 4);
+        TBitpack = TileDataPacking.SetBit(data.RedWire, TBitpack, 28);
+        TBitpack = TileDataPacking.SetBit(data.BlueWire, TBitpack, 29);
+        TBitpack = TileDataPacking.SetBit(data.GreenWire, TBitpack, 30);
+        TBitpack = TileDataPacking.SetBit(data.YellowWire, TBitpack, 31);
+    }
+}
