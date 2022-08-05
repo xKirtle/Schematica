3e932ca4fbfbf22a7a7b15ec195b5391e1bca9e1
Kirtle <xkirtle26@gmail.com>
2022-08-05 02:09:05 +0100

    Json Converters

---
+++ Core/Json Converters/TileDataConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Newtonsoft.Json;
+using Schematica.Common.DataStructures;
+
+namespace Schematica.Core.JsonConverters;
+
+public class TileDataConverter : JsonConverter<TileData>
+{
+    public override void WriteJson(JsonWriter writer, TileData tileData, JsonSerializer serializer) {
+        writer.WriteStartArray();
+        
+        writer.WriteValue(tileData.TType);
+        writer.WriteValue(tileData.WType);
+        writer.WriteValue(tileData.LAmount);
+        writer.WriteValue(tileData.LFlags);
+        writer.WriteValue(tileData.TFrameX);
+        writer.WriteValue(tileData.TFrameY);
+        writer.WriteValue(tileData.TBitpack);
+        
+        writer.WriteEndArray();
+    }
+
+    public override TileData ReadJson(JsonReader reader, Type objectType, TileData existingValue, bool hasExistingValue, JsonSerializer serializer) {
+        TileData tileData = new TileData();
+        
+        reader.Read(); // [
+        
+        tileData.TType = ushort.Parse(reader.ReadAsString());
+        tileData.WType = ushort.Parse(reader.ReadAsString());
+        tileData.LAmount = byte.Parse(reader.ReadAsString());
+        tileData.LFlags = byte.Parse(reader.ReadAsString());
+        tileData.TFrameX = short.Parse(reader.ReadAsString());
+        tileData.TFrameY = short.Parse(reader.ReadAsString());
+        tileData.TBitpack = int.Parse(reader.ReadAsString());
+        
+        reader.Read(); // ]
+        
+        return tileData;
+    }
+}
